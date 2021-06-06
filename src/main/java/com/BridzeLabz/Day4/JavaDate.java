c5318fe23b7c9beee8e47d0748e980dafd183f8b
Deepak Jaiswal <jaiswaldj96@gmail.com>
2021-06-06 22:27:19 +0530

    Updated Day 4

---
+++ src/main/java/com/BridzeLabz/Day4/JavaDate.java
@@ -0,0 +1,11 @@
+package com.BridzeLabz.Day4;
+
+import java.util.Date;
+
+public class JavaDate {
+
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println("day of the week is : "+d.getTime());
+    }
+}
---
+++ src/main/java/com/BridzeLabz/Day4/StdOut.java
@@ -0,0 +1,12 @@
+package com.BridzeLabz.Day4;
+
+public class StdOut {
+    public static void println(String s, int i) {
+    }
+
+    public static void println(String s) {
+    }
+
+    public static void println() {
+    }
+}
